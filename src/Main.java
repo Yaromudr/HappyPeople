66d187420ce90e1a3c42a90dff6c561250ca8cf5
Dolinskiy Aleksandr <xaker6662007@gmail.com>
2016-09-14 23:07:15 +0300

    Fix: BufferedReader to Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import static java.lang.String.format;
 
@@ -8,26 +10,24 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Enter path to .txt file: ");
+        Scanner scanner = new Scanner(System.in);
 
-       try(BufferedReader  consoleReader = new BufferedReader(new InputStreamReader(System.in))){
-           String path = consoleReader.readLine();
+        String path = scanner.nextLine();
 
-           String maxLengthStr = getMaxLengthStringFromFile(path);
-           char charStr = getCharAtMiddleIndex(maxLengthStr);
+        String maxLengthStr = getMaxLengthStringFromFile(path);
+        char charStr = getCharAtMiddleIndex(maxLengthStr);
 
-           System.out.println(format("Write a word that starts for: %s", charStr));
+        System.out.println(format("Write a word that starts for: %s", charStr));
 
-           String word = consoleReader.readLine();
-           check(charStr, word);
-       } catch (IOException e) {
-           System.err.println("Error in process reading from console");
-           e.printStackTrace();
-       }
+        String word = scanner.nextLine();
+        check(charStr, word);
 
+        scanner.close();
     }
 
     /**
      * Method receives address file in the file system and returns the string of the maximum length of the specified file
+     *
      * @param path address file in the file system
      * @return the string of the maximum length of the specified file
      */
@@ -36,29 +36,31 @@ public class Main {
         Map<String, Integer> map = new HashMap<>();
         File file = new File(path);
 
-       try(BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
-           String stringFromFile = null;
+        try (Scanner scanner = new Scanner(file)) {
+            String stringFromFile = null;
 
-           while ((stringFromFile = fileReader.readLine()) != null) {
-               stringFromFile = stringFromFile.trim().replace(" ", "");
-               map.put(stringFromFile, stringFromFile.length());
-           }
+            while (scanner.hasNext()) {
+                stringFromFile = scanner.nextLine();
+                stringFromFile = stringFromFile.trim().replace(" ", "");
+                map.put(stringFromFile, stringFromFile.length());
+            }
 
-           for (Map.Entry<String, Integer> entry : map.entrySet()) {
-               if (entry.getValue() > maxLengthStr.length())
-                   maxLengthStr = entry.getKey();
-           }
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > maxLengthStr.length())
+                    maxLengthStr = entry.getKey();
+            }
 
-           return maxLengthStr;
-       } catch (IOException e) {
-           System.err.println("Error in process reading file");
-           e.printStackTrace();
-           return null;
-       }
+            return maxLengthStr;
+        } catch (IOException e) {
+            System.err.println("Error in process reading file");
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /**
      * Method checks whether the word starts with the specified character and prints a message
+     *
      * @param charStr character
      * @param word
      */
@@ -71,6 +73,7 @@ public class Main {
 
     /**
      * Method calculates the index of the central line item and returns that element
+     *
      * @param string
      * @return central line item
      */
